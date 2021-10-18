e813ffb4e10a82eae0cc5e00bf2659c52630503c
dc4775 <dc4775@rit.edu>
2021-10-18 10:42:47 -0400

    Pickup test
    Script for pick up able object in the scene

--- Game2/Assets/Scripts/PickUp.cs
+++ Game2/Assets/Scripts/PickUp.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public Transform disLocation;
+
+    void OnMouseDown()
     {
-        
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = disLocation.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseUp()
     {
-        
+        this.transform.parent = null;
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
     }
 }
