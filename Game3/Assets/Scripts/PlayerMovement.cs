cbd273611faed51ea60013b73f28a18b67a327bf
Aidan Roberts <aidanroberts629@gmail.com>
2021-12-02 19:36:53 -0500

    added camera script, player controller/movement scripts

---
+++ Game3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // fields
+    public PlayerController controller; //accessing PlayerController script methods
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
