dbb0f7ffc448b0ac27cad4da008924d210e85b8c
Aidan Roberts <aidanroberts629@gmail.com>
2021-12-05 21:08:51 -0500

    ui updated, next level code added

--- Game3/Assets/Scripts/LevelEnd.cs
+++ Game3/Assets/Scripts/LevelEnd.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("trigger enter");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //loads next level in build index
     }
 }
