103ae0775dff98c8b6c507e9338b13b771c955db
Aidan Roberts <aidanroberts629@gmail.com>
2021-12-05 23:14:26 -0500

    added barebones start menu

--- Game3/Assets/Scripts/LevelEnd.cs
+++ Game3/Assets/Scripts/LevelEnd.cs
@@ -7,7 +7,6 @@ public class LevelEnd : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("trigger enter");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //loads next level in build index
     }
 }
