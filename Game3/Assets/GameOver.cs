dab0d78d200dfabbf121d98bda8f66530748819f
Aidan Roberts <aidanroberts629@gmail.com>
2021-12-06 14:30:53 -0500

    menu/deathbox changes

--- Game3/Assets/GameOver.cs
+++ Game3/Assets/GameOver.cs
@@ -7,7 +7,10 @@ public class GameOver : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player entered deathbox");
             SceneManager.LoadScene(4); //loads scene at index 4 (should be game over scene)
+        }
     }
 }
