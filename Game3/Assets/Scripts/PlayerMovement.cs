f974bd6d9035030702c9873d3c147813ec8abb24
Matthew Jacobs <jaj8571@rit.edu>
2021-12-05 23:35:44 -0500

    Rigged up Harry's animations
    Harry's animations now work

--- Game3/Assets/Scripts/PlayerMovement.cs
+++ Game3/Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,51 @@ public class PlayerMovement : MonoBehaviour
     public PlayerController controller; //accessing PlayerController script methods
 
     float horizontalMove = 0f;
+    float walkSet = 0f;
     public float runSpeed = 40f;
 
     bool jump = false;
     bool crouch = false;
 
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        walkSet = horizontalMove;
+
+        if (horizontalMove < 0)
+        {
+            walkSet = horizontalMove * -1;
+        }
+        _animator.SetFloat("walk", walkSet);
+
         if (Input.GetButtonDown("Jump"))
+        {
             jump = true;
+        }
+
 
         if (Input.GetButtonDown("Crouch"))
+        {
             crouch = true;
+
+        }
         else if (Input.GetButtonUp("Crouch"))
+        {
             crouch = false;
+        }
+
+        _animator.SetBool("crouch", crouch);
+        _animator.SetBool("crouchend", crouch);
+        _animator.SetBool("jump", jump);
     }
 
     void FixedUpdate()
